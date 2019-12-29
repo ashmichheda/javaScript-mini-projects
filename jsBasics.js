de8bf74b761f823ab37597cf367e01c967bdfd69
ashmichheda <ashmichheda@gmail.com>
2019-12-28 20:56:25 -0700

    added js file

---
+++ jsBasics.js
@@ -0,0 +1,27 @@
+// What is javascript?
+
+/*
+- A programming language that is used to make web pages interactive
+- It's an interpreted language
+- Run's on client's computer/browser
+- Object based
+- It's a "scripting" language and has nothing to do with Java
+*/
+
+/*
+Implemented the following programming fundamentals in JS
+1) Variables
+2) Arrays
+3) Loops
+4) Conditionals
+5) Comparison & Operators
+6) Objects
+7) Functions
+8) Program Flow
+9) Data types
+10) Prototypes
+11) Events
+*/
+
+// How to create JavaScript
+// You just need to create a text editor and run on any of the browser available.
