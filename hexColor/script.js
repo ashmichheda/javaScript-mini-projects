a9fad7fd47dcb88870942c5700ae11bb83f88394
ashmichheda <ashmichheda@gmail.com>
2020-01-09 23:08:04 -0700

    added hexColor files

---
+++ hexColor/script.js
@@ -0,0 +1,17 @@
+// Selecting button
+const colorBtn = document.querySelector('.colorButton');
+const bodyBcg = document.querySelector('body');
+
+// Declare an array of colors
+const colors = ['yellow', 'red', 'blue', 'green', '#5b6698'];
+
+// Use event listner to change color on select button
+colorBtn.addEventListener('click', changeColor);
+
+
+function changeColor(){
+
+  let random = Math.floor(Math.random()*colors.length);
+//  console.log("random: "+random);
+  bodyBcg.style.backgroundColor = colors[random];
+}
