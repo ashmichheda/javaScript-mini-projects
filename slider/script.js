65ed3174298920164d8fad530bb315c993c6dde1
ashmichheda <ashmichheda@gmail.com>
2020-01-16 08:16:39 -0700

    added slider project files

---
+++ slider/script.js
@@ -0,0 +1,33 @@
+let counter = document.querySelector('.counter');
+const lowerCount = document.querySelector('#lowerCountBtn');
+const addCount = document.querySelector('#addCountBtn');
+
+let count = 0;
+
+addCount.addEventListener('click', incrementCounter);
+lowerCount.addEventListener('click', decrementCounter);
+
+function incrementCounter(){
+  count++;
+  counter.innerHTML = count;
+  if(counter.innerHTML > '0'){
+    counter.style.color = 'yellow';
+  }
+  else if(counter.innerHTML == '0'){
+    counter.style.color = 'white';
+  }
+  counter.animate([{opacity:'0.2'},{opacity:'1.0'}],{duration:1000, fill: 'forwards'});
+}
+
+function decrementCounter(){
+  count--;
+  counter.innerHTML = count;
+  if(counter.innerHTML < '0'){
+    counter.style.color = 'red';
+  }
+  else if(counter.innerHTML == '0'){
+    counter.style.color = 'white';
+  }
+  // Using animation to slow down the counter occurrence
+  counter.animate([{opacity:'0.2'},{opacity:'1.0'}],{duration:1000, fill: 'forwards'});
+}
